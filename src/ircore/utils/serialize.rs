10a9a710e96121fd6483e5217219c594fb024440
binblee <binblee@users.noreply.github.com>
2022-10-20 19:46:31 +0800

    index file compress

--- src/ircore/utils/serialize.rs
+++ src/ircore/utils/serialize.rs
@@ -3,6 +3,9 @@ use std::fs::{self, File};
 use std::io::{self, Write, Read};
 use serde::{Serialize, Deserialize};
 use bincode::Options;
+use flate2::write::GzEncoder;
+use flate2::read::GzDecoder;
+use flate2::Compression;
 
 pub fn write_file<T: Serialize>(filepath: &Path, obj: &T) -> io::Result<()>{
     if let Some(dir) = filepath.parent() {
@@ -10,14 +13,16 @@ pub fn write_file<T: Serialize>(filepath: &Path, obj: &T) -> io::Result<()>{
     }
     let bincode_options = bincode::DefaultOptions::new().with_varint_encoding().allow_trailing_bytes();
     let encoded: Vec<u8> = bincode_options.serialize(obj).unwrap();
-    let mut writer = File::create(filepath)?;
+    let f = File::create(filepath)?;
+    let mut writer = GzEncoder::new(f, Compression::default());
     writer.write_all(&encoded)?;
     Ok(())
 }
 
 pub fn read_file<'a, T>(filepath: &Path, encoded: &'a mut Vec<u8>) -> io::Result<T>
     where T: Deserialize<'a> {
-    let mut reader = File::open(filepath).expect("cannot open file.");
+    let f = File::open(filepath).expect("cannot open file.");
+    let mut reader = GzDecoder::new(f);
     match reader.read_to_end(encoded){
         Ok(_) => {
             let bincode_options = bincode::DefaultOptions::new().with_varint_encoding().allow_trailing_bytes();
